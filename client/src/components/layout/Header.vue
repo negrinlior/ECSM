6c6dd33ddfe9bda25d5da860f4b738729c5904c4
negrinlior <negrinlior@gmail.com>
2019-06-03 08:39:35 +0300

    Vuetify with menu

--- client/src/components/layout/Header.vue
+++ client/src/components/layout/Header.vue
@@ -1,18 +1,77 @@
 <template>
-    <header class="header">
-        <H1>ECSM</H1>
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
-        </div>
-    </header>
+<div>
+    
+    <v-layout wrap style="height: 80px;">
+        <v-container>
+            <v-layout>
+                <v-btn
+                    color="pink"
+                    dark
+                    @click.stop="drawer = !drawer"
+                >
+                תפריט
+                </v-btn>
+                <H1>ECSM</H1>
+            </v-layout>       
+        </v-container>
+
+        <v-navigation-drawer
+            v-model="drawer"
+            absolute
+            temporary
+            >
+        <v-list class="pa-1">
+            <v-list-tile avatar>
+            <v-list-tile-avatar>
+                <img src="https://randomuser.me/api/portraits/men/85.jpg">
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+                <v-list-tile-title>User</v-list-tile-title>
+            </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+
+        <v-list class="pt-0" dense>
+            <v-divider></v-divider>
+
+            <v-list-tile
+                v-for="item in items"
+                :key="item.title"
+                @click=""
+                :to="item.adrs"
+            >
+                <v-list-tile-action>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-tile-action>
+
+                <v-list-tile-content>
+                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile-content>
+
+                  
+            </v-list-tile>
+        </v-list>
+        </v-navigation-drawer>
+    </v-layout>
+</div>
 </template>
 
 
-<<script>
+<script>
 export default {
-    name:"Header"
+     data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'בית', icon: 'dashboard', adrs: '/' },
+          { title: 'התחברות', icon: 'dashboard', adrs: '/login' },
+          { title: 'אודות', icon: 'question_answer', adrs: '/about' }
+        ]
+      }
+    }
 }
+
 </script>
 
 <style scoped>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <H1 align="center">התחברות</H1>
+        <v-form>
+             <v-flex xs12 sm6 md3>
+                <v-text-field
+                    type="username"
+                    v-model="username"
+                    label="שם משתמש"
+                    clearable
+                ></v-text-field>
+                <v-text-field
+                    type="password"
+                    v-model="password"
+                    label="סיסמא"
+                ></v-text-field>
+            </v-flex>
+           
+            <v-btn depressed small @click="Login">התחבר</v-btn>
+        
+        </v-form>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
