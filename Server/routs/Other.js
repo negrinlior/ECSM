bcab36ecc20d2d649273e81f997ebbac576738a4
negrinlior <negrinlior@gmail.com>
2020-06-06 20:12:43 +0300

    NikudScreenIsDone!

---
+++ Server/routs/Other.js
@@ -0,0 +1,12 @@
+
+function fFixDate(DtStr) {
+    var DtNd=DtStr.split('T')[0];
+    if (DtStr.split('T')[1].split(':')[0]!="00"){
+        var d=new Date(DtNd);
+        d.setDate(d.getDate() +1);
+        DtNd=d.toISOString().split('T')[0];
+    }
+    return DtNd;   // The function returns the product of p1 and p2
+  }
+ 
+module.exports.FixDate=fFixDate;
--- Server/routs/api/Nikud.js
+++ Server/routs/api/Nikud.js
@@ -2,19 +2,12 @@ const express=require('express');
 const DB=require('../DB');
 const config = require('../../config');
 const SQL=require("mssql");
+const OtherFunctions=require('../Other');
 
 const router=express.Router();
 const SelectQry=`SELECT * FROM ECSM_NikudSettingsView`;
 
-function FixDate(DtStr) {
-    var DtNd=DtStr.split('T')[0];
-    if (DtStr.split('T')[1].split(':')[0]!="00"){
-        var d=new Date(DtNd);
-        d.setDate(d.getDate() +1);
-        DtNd=d.toISOString().split('T')[0];
-    }
-    return DtNd;   // The function returns the product of p1 and p2
-  }
+
 router.get('/',async function(req,res){  
     try{
         var data=await DB.CommitSelectAndReturnRecordset(SelectQry);
@@ -37,30 +30,25 @@ router.post('/UpdateNikud',async function(req,res){
     try{
         var sqlreq= new SQL.Request();
         var Bdy=await req.body.value;
-        var DtStrt=Bdy.DateStart.split('T')[0];
-        console.log(Bdy);
         
-        sqlreq.input('ID',SQL.Int,Bdy.ID);
-        sqlreq.input('ObjectTypeDis',SQL.NVarChar,Bdy.ObjectType);
-        sqlreq.input('ObjectCodeDis',SQL.NVarChar,Bdy.ObjCodeDis);
-        sqlreq.input('DateStart',SQL.NVarChar,FixDate(Bdy.DateStart));
+        sqlreq.input('vID',SQL.Int,Bdy.ID);
+        sqlreq.input('vObjectTypeDis',SQL.NVarChar,Bdy.ObjectType);
+        sqlreq.input('vObjectCodeDis',SQL.NVarChar,Bdy.ObjCodeDis);
+        sqlreq.input('vDateStart',SQL.NVarChar,OtherFunctions.FixDate(Bdy.DateStart));
         if (Bdy.DateEnd){
-            sqlreq.input('DateEnd',SQL.NVarChar,DateStart);            
+            sqlreq.input('vDateEnd',SQL.NVarChar,OtherFunctions.FixDate(Bdy.DateEnd));            
         }
         else{
-            sqlreq.input('DateEnd',SQL.NVarChar,Bdy.DateEnd);
+            sqlreq.input('vDateEnd',SQL.NVarChar,Bdy.DateEnd);
         }
-        sqlreq.input('NikudType',SQL.Int,Bdy.NikudType);
-        sqlreq.input('NikudValue',SQL.Int,Bdy.NikudValue);
-        sqlreq.input('Machpil',SQL.Int,Bdy.Machpil);
-        sqlreq.input('MathAction',SQL.Int,Bdy.MathAction);
+        sqlreq.input('vNikudType',SQL.Int,Bdy.NikudType);
+        sqlreq.input('vNikudValue',SQL.NVarChar,Bdy.NikudValue);
+        sqlreq.input('vMachpil',SQL.NVarChar,Bdy.Machpil);
+        sqlreq.input('vMathAction',SQL.Int,Bdy.MathAction);
         
         sqlreq.output('Success', SQL.Int)
-        
-
 
          var data=await DB.ExecuteSP(sqlreq,"ECSM_NikudTableUpdate");
-        console.log(data);
 
          if (data.output.Success=1 && data.rowsAffected[0]>0){
              res.send(JSON.stringify(Bdy));
@@ -83,16 +71,23 @@ router.post('/InsertNikud',async function(req,res){
         var sqlreq= new SQL.Request();
         var Bdy=await req.body.value;
         
-        sqlreq.input('Customer',SQL.Int,Bdy.Customer);
-        sqlreq.input('Anaf',SQL.Int,Bdy.Anaf);
-        sqlreq.input('Kablan',SQL.Int,Bdy.Kablan);
-        sqlreq.input('Rashuiot',SQL.Int,Bdy.Rashuiot);
-        sqlreq.input('UserName',SQL.Int,Bdy.User1);
-        sqlreq.input('BackupUser',SQL.Int,Bdy.User2);
-        sqlreq.input('BackupUser2',SQL.Int,Bdy.User3);
+        sqlreq.input('vObjectTypeDis',SQL.NVarChar,Bdy.ObjectType);
+        sqlreq.input('vObjectCodeDis',SQL.NVarChar,Bdy.ObjCodeDis);
+        sqlreq.input('vDateStart',SQL.NVarChar,OtherFunctions.FixDate(Bdy.DateStart));
+        if (Bdy.DateEnd){
+            sqlreq.input('vDateEnd',SQL.NVarChar,OtherFunctions.FixDate(Bdy.DateEnd));            
+        }
+        else{
+            sqlreq.input('vDateEnd',SQL.NVarChar,Bdy.DateEnd);
+        }
+        sqlreq.input('vNikudType',SQL.Int,Bdy.NikudType);
+        sqlreq.input('vNikudValue',SQL.NVarChar,Bdy.NikudValue);
+        sqlreq.input('vMachpil',SQL.NVarChar,Bdy.Machpil);
+        sqlreq.input('vMathAction',SQL.Int,Bdy.MathAction);
+
         sqlreq.output('Success', SQL.Int)
 
-        var data=await DB.ExecuteSP(sqlreq,"ECSM_PermissionsTableInsert");
+        var data=await DB.ExecuteSP(sqlreq,"ECSM_NikudTableInsert");
         if (data.output.Success=1 && data.rowsAffected[0]>0){
             Bdy.ID=data.returnValue;
             res.send(JSON.stringify(Bdy));
@@ -117,7 +112,7 @@ router.post('/DeleteNikud',async function(req,res){
        
         sqlreq.output('Success', SQL.Int)
 
-        var data=await DB.ExecuteSP(sqlreq,"ECSM_PermissionsTableDelete");
+        var data=await DB.ExecuteSP(sqlreq,"ECSM_NikudTableDelete");
         if (data.output.Success=1 && data.rowsAffected[0]>0){
             
             res.send(JSON.stringify(Bdy));
